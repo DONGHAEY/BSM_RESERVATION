5365bf88237da2b5bfb017bb4bb41c439ba0d335
DONGHYEON <dongheaey@gmail.com>
2022-10-10 00:29:54 -0500

    env환경 시작

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,29 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '9310',
-      database: 'bssm_reservation',
-      synchronize: false,
-      entities: [__dirname + '/**/entity/*.entity.{js,ts}'],
-      logging: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./env/${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: 3306,
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME') || 'bssm_reservation',
+          synchronize: false,
+          entities: [__dirname + '/**/entity/*.entity.{js,ts}'],
+          logging: true,
+        };
+      },
     }),
     UserModule,
     AuthModule,
