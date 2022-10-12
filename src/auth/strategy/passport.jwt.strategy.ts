a45130299318d7090c9460758cee62b868fe03b9
DONGHYEON <dongheaey@gmail.com>
2022-10-11 22:04:55 -0500

    ADD CONFIG SERVICE FOR ENV AT passport.jwt.strategy

--- src/auth/strategy/passport.jwt.strategy.ts
+++ src/auth/strategy/passport.jwt.strategy.ts
@@ -4,22 +4,26 @@ import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
 import { AuthService } from '../auth.service';
 import { Request } from 'express';
 import { UserService } from 'src/user/user.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService,
+  ) {
     super({
       // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => request.cookies.accessToken,
       ]),
       ignoreExpiration: false, //만료기한을 무시할것인가
-      secretOrKey: 'ACCESSTOKEN_SECRET_KEY',
+      secretOrKey: configService.get('ACCESSTOKEN_SECRET_KEY'),
     });
   }
 
   async validate(payload, done: VerifiedCallback): Promise<any> {
-    const user = await this.userService.test(payload.userCode);
+    const user = await this.userService.testForFindUserByCode(payload.userCode);
     return done(null, user);
   }
 }
