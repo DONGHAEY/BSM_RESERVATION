e4938402c82eb45831bf2bd0104279c09fa42a18
DONGHYEON <dongheaey@gmail.com>
2022-10-12 21:15:54 -0500

    test

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { levelGuard } from 'src/auth/guards/level.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('room')
-@UseGuards(AuthGuard, levelGuard)
+@UseGuards(AuthGuard, levelGuard, RolesGuard)
 export class RoomController {}
