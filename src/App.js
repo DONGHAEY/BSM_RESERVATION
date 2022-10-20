5d2b982f78fc60e21bbb11d61d10a8c5b38fbc73
DONGHYEON <dongheaey@gmail.com>
2022-10-20 03:22:47 -0500

    fixed import error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AfterLogin from "./components/AfterLogin/AfterLogin";
 import Main from "./components/Main/Main";
 import {
   RecoilRoot,
