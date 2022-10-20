48b52c456c1dbce3c2a6c42b37dc61cc47b01164
DONGHYEON <dongheaey@gmail.com>
2022-10-20 04:17:44 -0500

    Main Page Layout

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -14,28 +14,30 @@ const Main = () => {
   const [user, setUser] = useRecoilState(userState);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    console.log(user);
-  }, []);
   return (
-    <div>
-      <h1>dd</h1>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <h1 style={{ fontSize: "10vw" }}>bsm_reservation</h1>
       {user.isLogin === false ? (
-        <a
-          href="https://auth.bssm.kro.kr/oauth?clientId=c53c85eb&redirectURI=http://localhost/api/oauth/bsm"
-          class="button accent"
-        >
-          BSM 계정으로 계속
-        </a>
+        <div>
+          <p>전자 이석증으로 베르실 예약을 간편하게..</p>
+          <a
+            href="https://auth.bssm.kro.kr/oauth?clientId=c53c85eb&redirectURI=http://localhost/api/oauth/bsm"
+            class="button accent"
+          >
+            BSM 계정으로 계속
+          </a>
+        </div>
       ) : (
-        <a
-          onClick={() => {
-            navigate("/service");
-          }}
-          class="button accent"
-        >
-          서비스로 가기
-        </a>
+        <div>
+          <a
+            onClick={() => {
+              navigate("/service");
+            }}
+            class="button accent"
+          >
+            서비스 이용 시작
+          </a>
+        </div>
       )}
     </div>
   );
