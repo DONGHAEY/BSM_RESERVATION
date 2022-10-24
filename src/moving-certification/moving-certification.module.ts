ed8baff70b4fe9ec2effd05a1e1d74be5c7d207e
DONGHYEON <dongheaey@gmail.com>
2022-10-23 20:13:12 -0500

    ADD definition

--- src/moving-certification/moving-certification.module.ts
+++ src/moving-certification/moving-certification.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RequestInfo } from './entity/RequestInfo.entity';
+import { RequestMember } from './entity/RequestMember.entity';
 import { MovingCertificationService } from './moving-certification.service';
 
 @Module({
-  providers: [MovingCertificationService]
+  imports: [TypeOrmModule.forFeature([RequestInfo, RequestMember])],
+  providers: [MovingCertificationService],
 })
 export class MovingCertificationModule {}
--- src/moving-certification/moving-certification.service.ts
+++ src/moving-certification/moving-certification.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RequestInfo } from './entity/RequestInfo.entity';
+import { Repository } from 'typeorm';
+import { RequestMember } from './entity/RequestMember.entity';
 
 @Injectable()
-export class MovingCertificationService {}
+export class MovingCertificationService {
+  constructor(
+    @InjectRepository(RequestInfo)
+    private requestInfoRepository: Repository<RequestInfo>,
+    @InjectRepository(RequestMember)
+    private requestMemberRepository: Repository<RequestMember>,
+  ) {}
+
+  //만들어야할 메서드 정리
+  // 1. 요청하기 기능
+  async request() {}
+  //
+  // 2. 이석증 인증/거부 하기 기능
+  async certificate() {}
+  //
+  // 3. 요청 했던 리스트 불러오기 기능
+  async getRequestList() {}
+  //
+}
